f4ccbe0455cf56963d7e385e3ea2f4a7ef7034e3
chris.webb <chris@mediumequalsmessage.com>
2011-11-13 16:21:46 +0100

    Initial Commit - broadening whats under git control

---
+++ sample_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '33257827a44840fbb15a8eed25001829fead3dfed2cb66b0a602560361400e93518fcf8fcb0ab538d5454d2811216e93367ed76e741abba6d56aa22f7bbc76c3'
