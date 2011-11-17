bb13cd0e4f4f47ecc7d6b5a87ab15870cf3b00b0
chris.webb <chris@mediumequalsmessage.com>
2011-11-17 21:03:52 +0100

    finished with routes and layouts

--- sample_app/spec/requests/layout_links_spec.rb
+++ sample_app/spec/requests/layout_links_spec.rb
@@ -19,5 +19,11 @@ describe "LayoutLinks" do
   	get '/help'
   	response.should have_selector('title', :content => "Help")
 
+  end 
+  it "shoud have a signup page at '/signup'" do
+    get '/signup'
+    response.should have_selector('title', :content => "Sign up")
+
   end
+
 end
