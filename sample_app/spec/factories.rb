ac0c828ad304bd619297ce01a9963df97ac740c1
chris.webb <chris@mediumequalsmessage.com>
2011-11-20 18:42:14 +0100

    finished user model and branch merge

---
+++ sample_app/spec/factories.rb
@@ -0,0 +1,7 @@
+# we use :user to get Factory Girl to simulate the user model.
+Factory.define :user do |user|
+	user.name			"Michael Hartl"
+	user.email			"mhartl@example.com"
+	user.password		"foobar"
+	user.password_confirmation "foobar"
+end
